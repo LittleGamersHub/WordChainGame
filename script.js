2381331952611eebcd7b911ac4f46c0c9010bbdb
LittleGamersHub <130539711+LittleGamersHub@users.noreply.github.com>
2023-04-12 12:56:29 +0300

    sd

--- script.js
+++ script.js
@@ -7,34 +7,105 @@ const startGameBtn = document.getElementById('start-game');
 let score = 0;
 let highestScore = 0;
 let gameInProgress = false;
+let usedWords = []; // Add this line
+let errorMessage = ""; // Add this line
+const startingWords = [
+    'apple', 'banana', 'coconut', 'dragonfruit', 'elephant', 'flamingo', 'giraffe', 'hippopotamus',
+    'iguana', 'jaguar', 'kangaroo', 'lemur', 'mongoose', 'narwhal', 'octopus', 'panda',
+    'quail', 'rabbit', 'seal', 'tiger'
+  ];
+
+function getRandomWord() {
+    
+    return startingWords[Math.floor(Math.random() * startingWords.length)];
+  }
+  
 
 function startGame() {
-    const startingWords = [
-      'apple', 'banana', 'coconut', 'dragonfruit', 'elephant', 'flamingo', 'giraffe', 'hippopotamus',
-      'iguana', 'jaguar', 'kangaroo', 'lemur', 'mongoose', 'narwhal', 'octopus', 'panda',
-      'quail', 'rabbit', 'seal', 'tiger'
-    ];
+
+    document.getElementById("game-over-message").style.display = "none";
     
     gameInProgress = true;
     score = 0;
     scoreSpan.textContent = score;
-    currentWord.textContent = startingWords[Math.floor(Math.random() * startingWords.length)];
+    currentWord.textContent = getRandomWord(); 
     inputWord.value = '';
     inputWord.focus();
+    usedWords = []; // Add this line
+    usedWords.push(currentWord.textContent.toLowerCase()); // Add this line
+  
 }
+
+
 
 function endGame() {
-  gameInProgress = false;
-  if (score > highestScore) {
-    highestScore = score;
-    highestScoreSpan.textContent = highestScore;
+    gameInProgress = false;
+    if (score > highestScore) {
+      highestScore = score;
+      highestScoreSpan.textContent = highestScore;
+    }
+    document.getElementById("game-over-message").innerHTML = `Game Over! ${errorMessage}`; // Update this line
+    document.getElementById("game-over-message").style.display = "block";
   }
-}
+  
 
-function isValidWord(word, prevWord) {
- 
-  return word.length > 0 && word[0].toLowerCase() === prevWord[prevWord.length - 1].toLowerCase();
-}
+  async function isValidWord(word, prevWord) {
+    const isRepeatingChar = (word) => {
+      for (let i = 1; i < word.length; i++) {
+        if (word[i].toLowerCase() !== word[i - 1].toLowerCase()) {
+          return false;
+        }
+      }
+      return true;
+    };
+  
+    if (word.length === 0) {
+      return "Please enter a word.";
+    }
+    if (isRepeatingChar(word)) {
+      return "Invalid word: consecutive repeating characters are not allowed.";
+    }
+    if (usedWords.includes(word.toLowerCase())) {
+      return "Invalid word: you have already used this word.";
+    }
+    if (word[0].toLowerCase() !== prevWord[prevWord.length - 1].toLowerCase()) {
+      return "Invalid word: the word should start with the last letter of the previous word.";
+    }
+  
+    const response = await fetch(
+      `https://api.datamuse.com/words?sp=${word.toLowerCase()}&max=1`
+    );
+    const data = await response.json();
+  
+    if (data.length === 0 || data[0].word.toLowerCase() !== word.toLowerCase()) {
+      return "Invalid word: the word is not recognized.";
+    }
+  
+    return "";
+  }
+  
+
+  async function submitWord() {
+    if (gameInProgress) {
+      const submitBtn = document.getElementById("submit-btn");
+      submitBtn.disabled = true; // Disable the submit button
+  
+      const validationResult = await isValidWord(inputWord.value, currentWord.textContent);
+      if (validationResult === "") {
+        currentWord.textContent = inputWord.value;
+        usedWords.push(inputWord.value.toLowerCase());
+        score++;
+        scoreSpan.textContent = score;
+      } else {
+        errorMessage = validationResult;
+        endGame();
+      }
+      inputWord.value = "";
+      submitBtn.disabled = false; // Enable the submit button
+    }
+  }
+  
+  
 
 inputWord.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
@@ -44,9 +115,9 @@ inputWord.addEventListener('keydown', (event) => {
 
 inputWord.addEventListener('keydown', (event) => {
   if (event.key === 'Enter' && gameInProgress) {
-    console.log('before is valid world');
+
     if (isValidWord(inputWord.value, currentWord.textContent)) {
-        console.log('is valid world');
+
       currentWord.textContent = inputWord.value;
       score++;
       scoreSpan.textContent = score;
@@ -57,19 +128,9 @@ inputWord.addEventListener('keydown', (event) => {
   }
 });
 
+window.addEventListener("load", startGame);
 
-function submitWord() {
-    if (gameInProgress) {
-      if (isValidWord(inputWord.value, currentWord.textContent)) {
-        currentWord.textContent = inputWord.value;
-        score++;
-        scoreSpan.textContent = score;
-      } else {
-        endGame();
-      }
-      inputWord.value = '';
-    }
-  }
+
 
 startGameBtn.addEventListener('click', startGame);
 
